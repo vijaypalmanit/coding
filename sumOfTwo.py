ecfca4ba316d9c044c6d6cc995f4431dfcc850e5
vijaypalmanit <vijaypalmanit@gmail.com>
2020-02-21 13:44:41 +0530

    adding new code

---
+++ sumOfTwo.py
@@ -0,0 +1,14 @@
+# two arrays and on target is given, return true if sum of two numbers (one from each array) from given array is equal to target.
+
+def sumOfTwo(a,b,target):
+	for i in a:
+		if target-i in b:
+			return True
+	return False
+	
+a=[5,8,9,2,3]
+b=[99,-10,3]
+target= -7
+
+print(sumOfTwo(a,b,target))
+	
