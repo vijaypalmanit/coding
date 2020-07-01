19a43dd52b04fca3988c9dfc7b03282b669540d0
vijaypalmanit <vijaypalmanit@gmail.com>
2020-07-02 00:53:42 +0530

    made changes and more user friendly

--- towerOfHanoi.py
+++ towerOfHanoi.py
@@ -4,18 +4,22 @@ Created on Tue Jun 30 23:59:30 2020
 
 @author: Vijay
 """
+
 total_move=0
 def hanoi(n,frm,via,to):
     if n==0:
         pass
     else:
         hanoi(n-1,frm,to,via)
-        print(f'move from {frm} to {to}')
+        
+        global total_move
+        print(f'{total_move+1}.move from {frm} --> {to}|')
+        
         total_move+=1
         hanoi(n-1,via,frm,to)
     
-hanoi(3,'A','B','C')
-print(total_move)    
+hanoi(4,'A','B','C')
+print(f'---------------------\ntotal move required here are :{total_move}')    
     
     
 
